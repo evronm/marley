acc6f0be76b1b791ff1f8f75d3560347088144a0
evronm <evronm@gmail.com>
2023-11-28 00:03:47 -0800

    fixed order of url parts retrieving rel options.

--- jamaica/jamaica.js
+++ jamaica/jamaica.js
@@ -73,7 +73,7 @@ function rowClick(e) {
 
 function showRels(e) {
   e.preventDefault();
-  var url=e.target.closest('form').action+"/rel_options/"+e.target.classList[1]+"/";
+  var url=e.target.closest('form').action+"/"+e.target.classList[1]+"/options";
   var div_id=e.target.classList[1]+"_list";
   u(e.target).after(div({id:div_id, class:"select"}));
   req(url)
