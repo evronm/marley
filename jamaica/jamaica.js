c8e5c93bd3ad61258f4cac402434f86b8e47d5f6
evronm <evronm@gmail.com>
2023-12-04 00:47:38 -0800

    reordered functions in jamaica.js; now hide add rel button on click

--- jamaica/jamaica.js
+++ jamaica/jamaica.js
@@ -18,6 +18,44 @@ function init() {
   u('#login').handle('click',login);
 }
 
+function show(target, json) {
+  u(target).empty().append(reggae2dom(json));
+  u('#main form').on('submit', formSubmit);
+  u('a').handle('click', urlClick)
+  u('.showRels').handle('click', showRels)
+  u('tr').handle('click', rowClick);
+}
+
+function req(url, meth, body) {
+  if (! meth) {meth="get"}
+  return fetch(url, {method: meth, headers: {'Authorization': 'Basic ' + getCreds()}, body: body})
+    .then((res) => res.json())
+}
+
+function val(name) {
+  return u('[name='+name+']').nodes[0].value;
+}
+
+function urlClick(e) {
+  req(e.target.href)
+    .then((json) => show('#main', json))
+    .catch((err) => console.error("error:", err));
+}
+
+function processResponse(json) {
+  if (json[0] == "error") {
+    showError(json);
+  } else {
+    show('#main', json);
+  }
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+  req(e.target.action, "POST", u(e.target).serialize()).
+    then((json) => processResponse(json)).
+    catch((err) => console.error("error:", err));
+}
 function login(e){
   e.preventDefault();
   fetch('/?nav', { headers: {'Authorization': 'Basic ' + getCreds()}})
@@ -44,22 +82,6 @@ function getCreds() {
   return btoa(val('eml') + ":" + val('pw'));
 }
 
-function val(name) {
-  return u('[name='+name+']').nodes[0].value;
-}
-
-function req(url, meth, body) {
-  if (! meth) {meth="get"}
-  return fetch(url, {method: meth, headers: {'Authorization': 'Basic ' + getCreds()}, body: body})
-    .then((res) => res.json())
-}
-
-function urlClick(e) {
-  req(e.target.href)
-    .then((json) => show('#main', json))
-    .catch((err) => console.error("error:", err));
-}
-
 function rowClick(e) {
   if (e.target.tagName == 'A') {return}
   var url = "/" + e.target.closest('table').classList[0] + "/" + u(e.target).parent('tr').attr('id');
@@ -70,6 +92,7 @@ function rowClick(e) {
 
 function showRels(e) {
   e.preventDefault();
+  e.target.hidden=true;
   var url=e.target.closest('form').action+"/"+e.target.classList[1]+"/options";
   var div_id=e.target.classList[1]+"_list";
   u(e.target).after(div({id:div_id, class:"select"}));
@@ -81,21 +104,6 @@ function showRels(e) {
     .catch((err) => console.error("error:", err));
 }
 
-function show(target, json) {
-  u(target).empty().append(reggae2dom(json));
-  u('#main form').on('submit', formSubmit);
-  u('a').handle('click', urlClick)
-  u('.showRels').handle('click', showRels)
-  u('tr').handle('click', rowClick);
-}
-
-function processResponse(json) {
-  if (json[0] == "error") {
-    showError(json);
-  } else {
-    show('#main', json);
-  }
-}
 
 function showError(json) {
   json.shift();
@@ -103,14 +111,6 @@ function showError(json) {
     u('[name=' + e[0] + ']').addClass('error').after(span({class: "err_msg"}, e[1]));
   }
 }
-
-function formSubmit(e) {
-  e.preventDefault();
-  req(e.target.action, "POST", u(e.target).serialize()).
-    then((json) => processResponse(json)).
-    catch((err) => console.error("error:", err));
-}
-
 
 window.onload=init;
 
