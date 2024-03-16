228628bb68ebba55910e9c90115fbee6ebb5cf56
evronm <evronm@gmail.com>
2024-03-16 13:07:26 -0700

    tags now show more or less correctly

--- jamaica/reggae_or.js
+++ jamaica/reggae_or.js
@@ -1,6 +1,12 @@
 Fields={
   tags: function (tags){
-    console.log(tags);
+    var options=[];
+    var val="";
+    tags.restrictions.options[2].forEach ((tag) =>{
+      options.push(tag[1]);
+      if(tags.val.includes(tag[0])) {val=val + tag[1] + " "}
+    })
+    return input({name: "tags", value: val}); 
   }
 
 }
